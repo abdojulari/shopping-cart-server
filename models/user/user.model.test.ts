ddd400ba398d1e123688460929010cfe12b8ad1e
Abdul Ojulari <abdulkabirojulari@gmail.com>
2023-09-20 18:31:27 -0600

    modify the test files

--- models/user/user.model.test.ts
+++ models/user/user.model.test.ts
@@ -1,41 +1,52 @@
-import { DataTypes } from "sequelize";
 import { User } from "./user.model";
 import * as bcrypt from 'bcrypt';
-import { test } from "../../databases/test-database";
 import { sequelize } from "../../databases/database";
 
 describe('User Model', () => {
   beforeAll(async () => {
-    // open connection to database 
-    await test.authenticate();
+
     // Recreate DB schema  
     await User.sync({ force: true }); 
     
   });
 
   beforeEach(async () => {
-   // await User.destroy({ truncate: true }); // Empty the table
+      // Empty the table after each test
 
+      await User.destroy({ truncate: true }); // 
   });
 
   it('should hash the password when setting', async () => {
     // hash the password using bcrypt
-
     const password = 'password123';
     const user = await User.create({
       name: 'John Tola',
       email: 'johndoe@example.com',
       password: bcrypt.hashSync(password, 10),
+      createAt: new Date(),
+      updatedAt: new Date()
     });
 
-    expect(user.getDataValue('password')).not.toBe('password123'); // Ensure password is hashed
+    expect(user.getDataValue('name')).toEqual('John Tola');
+    expect(user.getDataValue('email')).toEqual('johndoe@example.com');
   });
 
-  // Add more test cases for other aspects of your model as needed
+  // test if password isn't already hashed
+  it('should not hash the password if it is already hashed', async () => {
+      const password = 'password123';
+      const user = await User.create({
+        name: 'John Travis',
+        email: 'john.travis@example.com',
+        password: bcrypt.hashSync(password, 10),
+        createAt: new Date(),
+        updatedAt: new Date()
+      });
+
+      expect(user.getDataValue('password')).not.toBe('password123'); // Ensure password is hashed
+  });
 
   afterAll(async () => {
     // Close the Sequelize connection
-    await test.close(); 
     await sequelize.close();
   });
 });
